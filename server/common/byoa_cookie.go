c2e2aabfff0fca32464b8693194cfaf06f04dbf7
屁 <75117205+pixingzoudaiyuexing@users.noreply.github.com>
2026-09-03 19:30:10 +0800

    feat: write encrypted BYOA cookies

--- server/common/byoa_cookie.go
+++ server/common/byoa_cookie.go
@@ -9,10 +9,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// SetBYOACredentialCookie 将网盘凭据只写入当前浏览器的 HttpOnly Cookie。
+// SetBYOACredentialCookie 将网盘凭据加密后只写入当前浏览器的 HttpOnly Cookie。
 // 不写数据库、不写 Storage，也不把凭据返回给前端 JavaScript。
 func SetBYOACredentialCookie(c *gin.Context, provider byoa.Provider, credential string) error {
 	name, err := byoa.CookieName(provider)
+	if err != nil {
+		return err
+	}
+	encoded, err := byoa.EncodeCredential(provider, credential)
 	if err != nil {
 		return err
 	}
@@ -20,7 +24,7 @@ func SetBYOACredentialCookie(c *gin.Context, provider byoa.Provider, credential 
 	secure := c.Request.TLS != nil || strings.EqualFold(c.GetHeader("X-Forwarded-Proto"), "https")
 	http.SetCookie(c.Writer, &http.Cookie{
 		Name:     name,
-		Value:    byoa.EncodeCredential(credential),
+		Value:    encoded,
 		Path:     "/",
 		MaxAge:   int((7 * 24 * time.Hour).Seconds()),
 		HttpOnly: true,
